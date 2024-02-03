36f6df87151137d943590c697f429850c85f8da5
HaleYesilova <haleyesilova647@gmail.com>
2024-02-03 12:58:12 +0300

    Initial commit

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Logo from "../assets/images/logo.svg";
+
+function Nav() {
+    return (
+    <nav className="nav-wrapper">
+        <div className="nav-content">
+            <ul className="list-styled">
+                <li>
+                    <img src={Logo} alt="Apple" />
+                </li>
+                <li>
+                    <a className = "link-styled">Store</a>
+                </li>
+            </ul>
+        </div>
+    </nav>);
+}
+
+export default Nav;
