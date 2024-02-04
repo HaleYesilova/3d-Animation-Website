15b6375fafe49d066973bdf98cd9d40c862f1a9b
HaleYesilova <haleyesilova647@gmail.com>
2024-02-04 16:53:45 +0300

    WebGi viewer component implemented.

---
+++ src/components/WebgiViewer.jsx
@@ -0,0 +1,93 @@
+import React, {
+    useRef,
+    useState,
+    useCallback,
+    forwardRef,
+    useImperativeHandle,
+    useEffect,
+} from "react";
+import {
+    ViewerApp,
+    AssetManagerPlugin,
+    GBufferPlugin,
+    ProgressivePlugin,
+    TonemapPlugin,
+    SSRPlugin,
+    SSAOPlugin,
+    BloomPlugin,
+    GammaCorrectionPlugin,
+    mobileAndTabletCheck, //in mobile or in tablet
+} from "webgi";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+
+
+function WebgiViewer() {
+
+    const canvasRef = useRef(null);
+
+    const setupViewer = useCallback(async () => {
+         
+        // Initialize the viewer
+        const viewer = new ViewerApp({
+            canvas: canvasRef.current,
+        });
+
+        //adding the plugins
+        const manager = await viewer.addPlugin(AssetManagerPlugin);
+
+        const camera = viewer.scene.activeCamera;
+        const position = camera.position;
+        const target = camera.target;
+
+        // Add plugins individually.
+        await viewer.addPlugin(GBufferPlugin);
+        await viewer.addPlugin(new ProgressivePlugin(32));
+        await viewer.addPlugin(new TonemapPlugin(true));
+        await viewer.addPlugin(GammaCorrectionPlugin);
+        await viewer.addPlugin(SSRPlugin);
+        await viewer.addPlugin(SSAOPlugin);
+        // await viewer.addPlugin(DiamondPlugin)
+        // await viewer.addPlugin(FrameFadePlugin)
+        // await viewer.addPlugin(GLTFAnimationPlugin)
+        // await viewer.addPlugin(GroundPlugin)
+        await viewer.addPlugin(BloomPlugin);
+        // await viewer.addPlugin(TemporalAAPlugin)
+        // await viewer.addPlugin(AnisotropyPlugin)
+
+
+        viewer.renderer.refreshPipeline();
+
+        // Import and add a GLB file.
+        await manager.addFromPath("scene-black.glb");
+
+        viewer.getPlugin(TonemapPlugin).config.clipBackground = true;
+        
+        viewer.scene.activeCamera.setCameraOptions({ controlsEnabled: false});
+
+        window.scrollTo(0, 0);
+
+        let needsUpdate = true;
+
+        viewer.addEventListener("preFrame", () => {
+            if (needsUpdate){
+                camera.positionTargetUpdated(true);
+                needsUpdate = false;
+            }    
+        });
+
+
+    }, []);
+
+    useEffect(() => {
+        setupViewer();
+    }, []);
+
+
+    return (
+        <div id="webgi-canvas-container">
+            <canvas id="webgi-canvas" ref={canvasRef} /> /*rendering the 3d model inside this canvas*/
+        </div>
+    );
+}
+export default WebgiViewer;
